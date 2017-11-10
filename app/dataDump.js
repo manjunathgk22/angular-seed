d5c30935d90f1b94500c98bb3992a37c5c0f4be3
Manjunath <manjunathgk22@gmail.com>
2017-11-10 22:51:11 +0530

    Added dummy data for list

---
+++ app/dataDump.js
@@ -0,0 +1,502 @@
+var DataDump = [
+  {
+    "id": 1,
+    "name": "Nissan",
+    "avatar": "https://robohash.org/eiusdictaincidunt.png?size=50x50&set=set1"
+  },
+  {
+    "id": 2,
+    "name": "Oldsmobile",
+    "avatar": "https://robohash.org/velitsapientecommodi.jpg?size=50x50&set=set1"
+  },
+  {
+    "id": 3,
+    "name": "Ford",
+    "avatar": "https://robohash.org/corruptiinaliquid.jpg?size=50x50&set=set1"
+  },
+  {
+    "id": 4,
+    "name": "Cadillac",
+    "avatar": "https://robohash.org/utestut.png?size=50x50&set=set1"
+  },
+  {
+    "id": 5,
+    "name": "Jeep",
+    "avatar": "https://robohash.org/aaperiamlabore.png?size=50x50&set=set1"
+  },
+  {
+    "id": 6,
+    "name": "Toyota",
+    "avatar": "https://robohash.org/doloremeosassumenda.jpg?size=50x50&set=set1"
+  },
+  {
+    "id": 7,
+    "name": "Dodge",
+    "avatar": "https://robohash.org/quasifugacorrupti.jpg?size=50x50&set=set1"
+  },
+  {
+    "id": 8,
+    "name": "Ford",
+    "avatar": "https://robohash.org/consequaturhicest.jpg?size=50x50&set=set1"
+  },
+  {
+    "id": 9,
+    "name": "Mercedes-Benz",
+    "avatar": "https://robohash.org/voluptatesquiavoluptatem.bmp?size=50x50&set=set1"
+  },
+  {
+    "id": 10,
+    "name": "Mazda",
+    "avatar": "https://robohash.org/sintrepellenduseaque.bmp?size=50x50&set=set1"
+  },
+  {
+    "id": 11,
+    "name": "GMC",
+    "avatar": "https://robohash.org/idliberovoluptatem.png?size=50x50&set=set1"
+  },
+  {
+    "id": 12,
+    "name": "Toyota",
+    "avatar": "https://robohash.org/veniamidex.jpg?size=50x50&set=set1"
+  },
+  {
+    "id": 13,
+    "name": "Volvo",
+    "avatar": "https://robohash.org/itaquemaximeblanditiis.jpg?size=50x50&set=set1"
+  },
+  {
+    "id": 14,
+    "name": "Ferrari",
+    "avatar": "https://robohash.org/istedistinctioin.png?size=50x50&set=set1"
+  },
+  {
+    "id": 15,
+    "name": "Mercedes-Benz",
+    "avatar": "https://robohash.org/autverorepellendus.bmp?size=50x50&set=set1"
+  },
+  {
+    "id": 16,
+    "name": "Mercedes-Benz",
+    "avatar": "https://robohash.org/nobisdoloresomnis.bmp?size=50x50&set=set1"
+  },
+  {
+    "id": 17,
+    "name": "Subaru",
+    "avatar": "https://robohash.org/seddoloret.jpg?size=50x50&set=set1"
+  },
+  {
+    "id": 18,
+    "name": "Lexus",
+    "avatar": "https://robohash.org/laudantiumnihilvoluptates.png?size=50x50&set=set1"
+  },
+  {
+    "id": 19,
+    "name": "Ford",
+    "avatar": "https://robohash.org/porrorerumdolor.bmp?size=50x50&set=set1"
+  },
+  {
+    "id": 20,
+    "name": "Audi",
+    "avatar": "https://robohash.org/sequisuntdignissimos.png?size=50x50&set=set1"
+  },
+  {
+    "id": 21,
+    "name": "Nissan",
+    "avatar": "https://robohash.org/earumiuredolore.png?size=50x50&set=set1"
+  },
+  {
+    "id": 22,
+    "name": "BMW",
+    "avatar": "https://robohash.org/laborummaximemolestiae.bmp?size=50x50&set=set1"
+  },
+  {
+    "id": 23,
+    "name": "Infiniti",
+    "avatar": "https://robohash.org/possimusdoloresquaerat.png?size=50x50&set=set1"
+  },
+  {
+    "id": 24,
+    "name": "Volvo",
+    "avatar": "https://robohash.org/etatquevoluptatem.png?size=50x50&set=set1"
+  },
+  {
+    "id": 25,
+    "name": "Infiniti",
+    "avatar": "https://robohash.org/natustemporeest.bmp?size=50x50&set=set1"
+  },
+  {
+    "id": 26,
+    "name": "GMC",
+    "avatar": "https://robohash.org/laboriosamminimamaxime.bmp?size=50x50&set=set1"
+  },
+  {
+    "id": 27,
+    "name": "Plymouth",
+    "avatar": "https://robohash.org/velconsequunturin.jpg?size=50x50&set=set1"
+  },
+  {
+    "id": 28,
+    "name": "Chevrolet",
+    "avatar": "https://robohash.org/harumtemporesimilique.bmp?size=50x50&set=set1"
+  },
+  {
+    "id": 29,
+    "name": "Mitsubishi",
+    "avatar": "https://robohash.org/evenietconsequunturnecessitatibus.jpg?size=50x50&set=set1"
+  },
+  {
+    "id": 30,
+    "name": "Jeep",
+    "avatar": "https://robohash.org/idperspiciatisnon.png?size=50x50&set=set1"
+  },
+  {
+    "id": 31,
+    "name": "GMC",
+    "avatar": "https://robohash.org/uteumid.jpg?size=50x50&set=set1"
+  },
+  {
+    "id": 32,
+    "name": "Nissan",
+    "avatar": "https://robohash.org/nullaexcepturirepellendus.bmp?size=50x50&set=set1"
+  },
+  {
+    "id": 33,
+    "name": "Porsche",
+    "avatar": "https://robohash.org/ducimusautreprehenderit.jpg?size=50x50&set=set1"
+  },
+  {
+    "id": 34,
+    "name": "Chevrolet",
+    "avatar": "https://robohash.org/doloribuscupiditateid.bmp?size=50x50&set=set1"
+  },
+  {
+    "id": 35,
+    "name": "Subaru",
+    "avatar": "https://robohash.org/delectusnamdolor.png?size=50x50&set=set1"
+  },
+  {
+    "id": 36,
+    "name": "Suzuki",
+    "avatar": "https://robohash.org/commodivoluptasadipisci.png?size=50x50&set=set1"
+  },
+  {
+    "id": 37,
+    "name": "Dodge",
+    "avatar": "https://robohash.org/nonbeataeminus.png?size=50x50&set=set1"
+  },
+  {
+    "id": 38,
+    "name": "Pontiac",
+    "avatar": "https://robohash.org/doloremeossed.png?size=50x50&set=set1"
+  },
+  {
+    "id": 39,
+    "name": "Buick",
+    "avatar": "https://robohash.org/porroautemodio.jpg?size=50x50&set=set1"
+  },
+  {
+    "id": 40,
+    "name": "Mercedes-Benz",
+    "avatar": "https://robohash.org/estrepellendusqui.png?size=50x50&set=set1"
+  },
+  {
+    "id": 41,
+    "name": "Porsche",
+    "avatar": "https://robohash.org/velitnonquis.jpg?size=50x50&set=set1"
+  },
+  {
+    "id": 42,
+    "name": "Oldsmobile",
+    "avatar": "https://robohash.org/aullamet.bmp?size=50x50&set=set1"
+  },
+  {
+    "id": 43,
+    "name": "Ford",
+    "avatar": "https://robohash.org/autemdelectusdoloremque.png?size=50x50&set=set1"
+  },
+  {
+    "id": 44,
+    "name": "Mercedes-Benz",
+    "avatar": "https://robohash.org/doloresadipisciunde.png?size=50x50&set=set1"
+  },
+  {
+    "id": 45,
+    "name": "Buick",
+    "avatar": "https://robohash.org/repellatdebitisearum.png?size=50x50&set=set1"
+  },
+  {
+    "id": 46,
+    "name": "Audi",
+    "avatar": "https://robohash.org/quaeevenietautem.jpg?size=50x50&set=set1"
+  },
+  {
+    "id": 47,
+    "name": "Volkswagen",
+    "avatar": "https://robohash.org/sedetlaboriosam.bmp?size=50x50&set=set1"
+  },
+  {
+    "id": 48,
+    "name": "Chevrolet",
+    "avatar": "https://robohash.org/errorrepellatsit.jpg?size=50x50&set=set1"
+  },
+  {
+    "id": 49,
+    "name": "Saturn",
+    "avatar": "https://robohash.org/sitomnisdolorem.jpg?size=50x50&set=set1"
+  },
+  {
+    "id": 50,
+    "name": "Saab",
+    "avatar": "https://robohash.org/doloresnesciuntadipisci.bmp?size=50x50&set=set1"
+  },
+  {
+    "id": 51,
+    "name": "Nissan",
+    "avatar": "https://robohash.org/voluptateminciduntqui.png?size=50x50&set=set1"
+  },
+  {
+    "id": 52,
+    "name": "Ram",
+    "avatar": "https://robohash.org/aperiammagniipsum.bmp?size=50x50&set=set1"
+  },
+  {
+    "id": 53,
+    "name": "Mercedes-Benz",
+    "avatar": "https://robohash.org/molestiaevoluptatumest.bmp?size=50x50&set=set1"
+  },
+  {
+    "id": 54,
+    "name": "Nissan",
+    "avatar": "https://robohash.org/voluptasnemovoluptas.png?size=50x50&set=set1"
+  },
+  {
+    "id": 55,
+    "name": "Lotus",
+    "avatar": "https://robohash.org/eaundedistinctio.bmp?size=50x50&set=set1"
+  },
+  {
+    "id": 56,
+    "name": "Suzuki",
+    "avatar": "https://robohash.org/inventoreutprovident.png?size=50x50&set=set1"
+  },
+  {
+    "id": 57,
+    "name": "Buick",
+    "avatar": "https://robohash.org/doloresaliasullam.png?size=50x50&set=set1"
+  },
+  {
+    "id": 58,
+    "name": "Cadillac",
+    "avatar": "https://robohash.org/temporequasdistinctio.jpg?size=50x50&set=set1"
+  },
+  {
+    "id": 59,
+    "name": "Mazda",
+    "avatar": "https://robohash.org/modiatquesoluta.jpg?size=50x50&set=set1"
+  },
+  {
+    "id": 60,
+    "name": "Ford",
+    "avatar": "https://robohash.org/placeatvoluptasperspiciatis.bmp?size=50x50&set=set1"
+  },
+  {
+    "id": 61,
+    "name": "Mitsubishi",
+    "avatar": "https://robohash.org/blanditiisdeseruntimpedit.jpg?size=50x50&set=set1"
+  },
+  {
+    "id": 62,
+    "name": "Porsche",
+    "avatar": "https://robohash.org/dolormolestiaeaut.png?size=50x50&set=set1"
+  },
+  {
+    "id": 63,
+    "name": "Chrysler",
+    "avatar": "https://robohash.org/etquidolor.png?size=50x50&set=set1"
+  },
+  {
+    "id": 64,
+    "name": "Jaguar",
+    "avatar": "https://robohash.org/etetquae.jpg?size=50x50&set=set1"
+  },
+  {
+    "id": 65,
+    "name": "Nissan",
+    "avatar": "https://robohash.org/sequirepellendusveniam.jpg?size=50x50&set=set1"
+  },
+  {
+    "id": 66,
+    "name": "Ford",
+    "avatar": "https://robohash.org/etmollitiatempora.png?size=50x50&set=set1"
+  },
+  {
+    "id": 67,
+    "name": "Ford",
+    "avatar": "https://robohash.org/iureofficiisaut.jpg?size=50x50&set=set1"
+  },
+  {
+    "id": 68,
+    "name": "GMC",
+    "avatar": "https://robohash.org/aliquamnesciuntnon.jpg?size=50x50&set=set1"
+  },
+  {
+    "id": 69,
+    "name": "Chevrolet",
+    "avatar": "https://robohash.org/estexcepturiesse.bmp?size=50x50&set=set1"
+  },
+  {
+    "id": 70,
+    "name": "Toyota",
+    "avatar": "https://robohash.org/sedatvitae.bmp?size=50x50&set=set1"
+  },
+  {
+    "id": 71,
+    "name": "Nissan",
+    "avatar": "https://robohash.org/sitdoloremqueeaque.png?size=50x50&set=set1"
+  },
+  {
+    "id": 72,
+    "name": "Maserati",
+    "avatar": "https://robohash.org/doloresvoluptatibusdistinctio.jpg?size=50x50&set=set1"
+  },
+  {
+    "id": 73,
+    "name": "Hummer",
+    "avatar": "https://robohash.org/suscipitdoloremalias.bmp?size=50x50&set=set1"
+  },
+  {
+    "id": 74,
+    "name": "Toyota",
+    "avatar": "https://robohash.org/nobisconsequaturtenetur.jpg?size=50x50&set=set1"
+  },
+  {
+    "id": 75,
+    "name": "Ford",
+    "avatar": "https://robohash.org/temporibusveritatiscorporis.bmp?size=50x50&set=set1"
+  },
+  {
+    "id": 76,
+    "name": "Mitsubishi",
+    "avatar": "https://robohash.org/fugiatdolorcorrupti.bmp?size=50x50&set=set1"
+  },
+  {
+    "id": 77,
+    "name": "Audi",
+    "avatar": "https://robohash.org/commodienimsunt.png?size=50x50&set=set1"
+  },
+  {
+    "id": 78,
+    "name": "Jaguar",
+    "avatar": "https://robohash.org/quipraesentiumneque.jpg?size=50x50&set=set1"
+  },
+  {
+    "id": 79,
+    "name": "Dodge",
+    "avatar": "https://robohash.org/autconsequaturhic.bmp?size=50x50&set=set1"
+  },
+  {
+    "id": 80,
+    "name": "Kia",
+    "avatar": "https://robohash.org/blanditiisinventoreerror.jpg?size=50x50&set=set1"
+  },
+  {
+    "id": 81,
+    "name": "Chevrolet",
+    "avatar": "https://robohash.org/impeditaccusantiumexplicabo.png?size=50x50&set=set1"
+  },
+  {
+    "id": 82,
+    "name": "Chevrolet",
+    "avatar": "https://robohash.org/oditutaut.png?size=50x50&set=set1"
+  },
+  {
+    "id": 83,
+    "name": "Mercury",
+    "avatar": "https://robohash.org/undemaximeconsequuntur.png?size=50x50&set=set1"
+  },
+  {
+    "id": 84,
+    "name": "Mazda",
+    "avatar": "https://robohash.org/repudiandaeautex.bmp?size=50x50&set=set1"
+  },
+  {
+    "id": 85,
+    "name": "Subaru",
+    "avatar": "https://robohash.org/undeillumlaboriosam.png?size=50x50&set=set1"
+  },
+  {
+    "id": 86,
+    "name": "Chevrolet",
+    "avatar": "https://robohash.org/quidemtenetursit.png?size=50x50&set=set1"
+  },
+  {
+    "id": 87,
+    "name": "Oldsmobile",
+    "avatar": "https://robohash.org/modiillosoluta.jpg?size=50x50&set=set1"
+  },
+  {
+    "id": 88,
+    "name": "Mercury",
+    "avatar": "https://robohash.org/quisoptionon.jpg?size=50x50&set=set1"
+  },
+  {
+    "id": 89,
+    "name": "Audi",
+    "avatar": "https://robohash.org/eiusutquidem.jpg?size=50x50&set=set1"
+  },
+  {
+    "id": 90,
+    "name": "Pontiac",
+    "avatar": "https://robohash.org/itaqueeaquenumquam.png?size=50x50&set=set1"
+  },
+  {
+    "id": 91,
+    "name": "Audi",
+    "avatar": "https://robohash.org/uthicrecusandae.png?size=50x50&set=set1"
+  },
+  {
+    "id": 92,
+    "name": "Ford",
+    "avatar": "https://robohash.org/ipsamprovidentet.bmp?size=50x50&set=set1"
+  },
+  {
+    "id": 93,
+    "name": "GMC",
+    "avatar": "https://robohash.org/voluptatemrecusandaeet.jpg?size=50x50&set=set1"
+  },
+  {
+    "id": 94,
+    "name": "Chevrolet",
+    "avatar": "https://robohash.org/quiestiusto.jpg?size=50x50&set=set1"
+  },
+  {
+    "id": 95,
+    "name": "Buick",
+    "avatar": "https://robohash.org/officiafugarepellat.jpg?size=50x50&set=set1"
+  },
+  {
+    "id": 96,
+    "name": "Subaru",
+    "avatar": "https://robohash.org/evenietadolor.jpg?size=50x50&set=set1"
+  },
+  {
+    "id": 97,
+    "name": "Mercedes-Benz",
+    "avatar": "https://robohash.org/eumquaeratquae.png?size=50x50&set=set1"
+  },
+  {
+    "id": 98,
+    "name": "Toyota",
+    "avatar": "https://robohash.org/quamearumprovident.bmp?size=50x50&set=set1"
+  },
+  {
+    "id": 99,
+    "name": "Nissan",
+    "avatar": "https://robohash.org/oditquideleniti.png?size=50x50&set=set1"
+  },
+  {
+    "id": 100,
+    "name": "Cadillac",
+    "avatar": "https://robohash.org/dictaaperiamnihil.bmp?size=50x50&set=set1"
+  }
+]
